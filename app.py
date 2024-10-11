7c8120eb6f22426fad632522b4358c1869ff4a01
Sanjeev <premchandrasingh786@gmail.com>
2024-10-11 19:30:07 +0530

    Yolo 11 version added

--- app.py
+++ app.py
@@ -4,15 +4,29 @@ from PIL import Image
 import numpy as np
 
 # Set up the Streamlit app
-st.title("YOLOv10 Object Detection App")
-st.write("Upload an image to perform object detection using YOLOv10")
+st.title("YOLO Object Detection App")
+st.write("Upload an image to perform object detection using YOLO")
+
+# Sidebar for selecting YOLO version
+yolo_version = st.sidebar.selectbox(
+    "Select YOLO Version",
+    ["YOLOv10", "YOLOv11"],
+    index=1
+)
 
 # Sidebar for selecting model and threshold
-modelName = st.sidebar.selectbox(
-    "Select YOLOv10 Model",
-    ["yolov10n.pt", "yolov10s.pt", "yolov10m.pt","yolov10b.pt", "yolov10l.pt", "yolov10x.pt"],
-    index=0
-)
+if yolo_version == "YOLOv10":
+    modelName = st.sidebar.selectbox(
+        "Select YOLOv10 Model",
+        ["yolov10n.pt", "yolov10s.pt", "yolov10m.pt", "yolov10l.pt", "yolov10x.pt"],
+        index=0
+    )
+else:
+    modelName = st.sidebar.selectbox(
+        "Select YOLOv11 Model",
+        ["yolo11n.pt", "yolo11s.pt", "yolo11m.pt", "yolo11b.pt", "yolo11l.pt", "yolo11x.pt"],
+        index=0
+    )
 
 threshold = st.sidebar.slider("Detection Threshold", 0.0, 1.0, 0.5, 0.05)
 
@@ -29,7 +43,7 @@ if uploaded_image is not None:
     detector.downloadModel(modelName)
 
     # Predict the image
-    st.write("Running YOLOv10...")
+    st.write(f"Running {yolo_version}...")
     result_image = detector.createBoundingBox(image_np, threshold)
 
     # Display the result image
